d2338a6ea4fd65dc438fd97b97018d3bf7f90fa3
Javad Mohammadzadeh <javad.ir.1384@gmail.com>
2025-02-20 10:53:11 +0330

    Sum Number

--- 01C.js
+++ 01C.js
@@ -1,1 +1,4 @@
-console.log(Number(process.argv[2]) + Number(process.argv[3]));
+const number1 = Number(process.argv[2])
+const number2 = Number(process.argv[3])
+const sum = number1 + number2
+console.log(sum)
